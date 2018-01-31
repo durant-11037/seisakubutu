e70dbc098155a9f38965b66cf8e124c12837bba4
internousdevwork <dangandurant@gmail.com>
2018-01-31 16:06:01 +0900

    01/31

--- repos/Project1/Project1/Source.cpp
+++ repos/Project1/Project1/Source.cpp
@@ -28,15 +28,65 @@ void CoC(void) {
 	}
 }
 
+void Shirobi(void) {
+	int x, y, z;
+	int i[] = {0, 0};
+	printf("roll?\n");
+	printf("2D6<="); scanf("%d", &x);
+	for(y = 0; y < 2; y++){
+	init_genrand((unsigned)time(NULL));
+	i[y] = genrand_int32()%6+1;
+	z += i[y];
+	}
+	
+	if (z > x) {
+		if (z == 12) {
+			printf("6,6 → 12 クリティカル\n");
+		}
+		else {
+			for(y = 0; y < 2; y++){
+			if(y == 1){
+				printf("%d", i[y]);
+			}else{
+			printf("%d,", i[y]);
+			}
+			printf("→ %d 成功\n", z);
+			}
+			
+		}
+	}else if (i < x) {
+		if (i == 2) {
+			printf("1,1 → 2 ファンブル\n");
+		}
+		else {
+			for(y = 0; y < 2; y++){
+			if(y == 1){
+				printf("%d", i[y]);
+			}else{
+			printf("%d,", i[y]);
+			}
+			printf("→ %d 失敗\n", z);
+			}
+		}
+	}
+}
+
 int main(void) {
 	int a, b;
 	do {
-		printf("モード選択:1D100…0 exit…9:"); scanf("%d", &a);
+		printf("モード選択:1D100…0 2D6…1 exit…9:"); scanf("%d", &a);
 		printf("\n");
 		switch(a){
 			case 0:
 			do {
 				CoC();
+				printf("戻る場合は0:"); scanf("%d", &b);
+				printf("\n");
+			} while (!(b == 0));
+			break;
+			case 1:
+			do {
+				Shinobi();
 				printf("戻る場合は0:"); scanf("%d", &b);
 				printf("\n");
 			} while (!(b == 0));
